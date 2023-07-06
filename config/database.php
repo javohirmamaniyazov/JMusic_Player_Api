0d832523d825af2383adadf2089f3f14f2d59d4c
javohir <javohirmamaniyazov@gmail.com>
2023-07-06 09:03:53 +0500

    changed again config/database

--- config/database.php
+++ config/database.php
@@ -45,11 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('RDS_HOSTNAME', '127.0.0.1'),
-            'port' => env('RDS_PORT', '3306'),
-            'database' => env('RDS_DATABASE', 'forge'),
-            'username' => env('RDS_USERNAME', 'forge'),
-            'password' => env('RDS_PASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
